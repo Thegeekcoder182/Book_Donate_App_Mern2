2e03be63eba82680e960f04066d5463fd499db18
Ayan Barai <105867399+Thegeekcoder182@users.noreply.github.com>
2023-12-30 19:10:40 +0530

    Table.js

--- Client/src/components/Table/Table.js
+++ Client/src/components/Table/Table.js
@@ -1,11 +1,16 @@
+// Importing necessary modules and styles
 import React from 'react';
 import './Table.css';
 
+// Table component for displaying and editing book data
 function Table({ data, onEdit, onUpdate, onDelete, isTableEditable }) {
+  // Function to check if a value is a number
   const isNumber = (value) => /^\d+$/.test(value);
 
+  // Rendering the Table component
   return (
     <div className="table_container">
+      {/* Table structure */}
       <table id="data_table">
         {/* Table headers */}
         <thead>
@@ -24,19 +29,38 @@ function Table({ data, onEdit, onUpdate, onDelete, isTableEditable }) {
         <tbody>
           {data.map((book, index) => (
             <tr key={index}>
+              {/* Book No */}
               <td>{book.sno}</td>
+              
+              {/* Title */}
               <td contentEditable={book.editable}>{book.title}</td>
+              
+              {/* Author */}
               <td contentEditable={book.editable}>{book.author}</td>
+              
+              {/* Genre */}
               <td contentEditable={book.editable}>{book.genre}</td>
-              <td contentEditable={book.editable && isTableEditable}>{isNumber(book.yop) ? book.yop : ''}</td>
-              <td contentEditable={book.editable && isTableEditable}>{isNumber(book.isbn) ? book.isbn : ''}</td>
+              
+              {/* Year of Publication */}
+              <td contentEditable={book.editable && isTableEditable}>
+                {isNumber(book.yop) ? book.yop : ''}
+              </td>
+              
+              {/* ISBN */}
+              <td contentEditable={book.editable && isTableEditable}>
+                {isNumber(book.isbn) ? book.isbn : ''}
+              </td>
+              
+              {/* Actions column */}
               <td>
                 {book.editable ? (
+                  // Save and Cancel buttons when in editable mode
                   <React.Fragment>
                     <button onClick={() => onUpdate(index)}>Save</button>
                     <button onClick={() => onEdit(index)}>Cancel</button>
                   </React.Fragment>
                 ) : (
+                  // Edit and Delete buttons when not in editable mode
                   <React.Fragment>
                     <button onClick={() => onEdit(index)}>Edit</button>
                     <button onClick={() => onDelete(index)}>Delete</button>
@@ -51,4 +75,5 @@ function Table({ data, onEdit, onUpdate, onDelete, isTableEditable }) {
   );
 }
 
+// Exporting the Table component
 export default Table;
