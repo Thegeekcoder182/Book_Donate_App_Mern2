c21af74c72bcae726a2533847890dc6c5d84f9b2
Ayan Barai <105867399+Thegeekcoder182@users.noreply.github.com>
2023-12-30 18:47:34 +0530

    App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,3 +1,4 @@
+// Importing necessary modules and components
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from './components/Form/Form';
@@ -6,28 +7,37 @@ import Auth from './components/Auth/Auth';
 import Axios from 'axios';
 import { auth } from './components/firebase/firebase';
 
+// Main App component
 function App() {
+  // State to manage user data (name, email, phone)
   const [userData, setUserData] = useState({
     name: '',
     email: '',
     phone: '',
   });
 
+  // State to manage books data
   const [booksData, setBooksData] = useState([]);
 
+  // State to manage user authentication status
   const [user, setUser] = useState(null);
 
+  // useEffect to handle authentication changes and load books when user is logged in
   useEffect(() => {
+    // Subscribe to authentication state changes
     const unsubscribe = auth.onAuthStateChanged((currentUser) => {
       setUser(currentUser);
       if (currentUser) {
+        // If user is logged in, load books
         loadBooks();
       }
     });
 
+    // Unsubscribe when component unmounts
     return () => unsubscribe();
   }, [user]);
 
+  // Function to fetch books data from the server
   const loadBooks = () => {
     Axios.get('http://localhost:5000/books')
       .then((response) => {
@@ -42,15 +52,17 @@ function App() {
       });
   };
 
+  // Function to handle form submission
   const handleFormSubmit = (formData) => {
     console.log('Form data submitted:', formData);
     setUserData(formData);
   };
 
+  // Function to handle adding a new book
   const handleAddClick = () => {
     if (user) {
       const newBook = {
-        sno: booksData.length + 1,  // Generate Book No based on current length
+        sno: booksData.length + 1, // Generate Book No based on current length
         title: '',
         author: '',
         genre: '',
@@ -64,29 +76,35 @@ function App() {
         editable: true,
       };
 
+      // Update booksData with the new book
       setBooksData((prevBooks) => [...prevBooks, newBook]);
     } else {
+      // Alert if user is not authenticated
       alert('You need to be authenticated to add a new book.');
     }
-  }
+  };
 
+  // Function to handle editing a row in the table
   const handleEditRow = (index) => {
     const updatedBooks = [...booksData];
     updatedBooks[index].editable = !updatedBooks[index].editable;
     setBooksData(updatedBooks);
   };
 
+  // Function to handle updating a row in the table
   const handleUpdateRow = (index) => {
     const updatedBooks = [...booksData];
     updatedBooks[index].editable = false;
     setBooksData(updatedBooks);
   };
 
+  // Function to handle deleting a row in the table
   const handleDeleteRow = (index) => {
     const updatedBooks = booksData.filter((book, i) => i !== index);
     setBooksData(updatedBooks);
   };
 
+  // Function to dump state to JSON format
   const handleDumpStateToJson = () => {
     const userState = {
       name: userData.name,
@@ -102,15 +120,18 @@ function App() {
       })),
     };
 
+    // Convert user state to JSON and display it in the console
     const jsonString = JSON.stringify(userState, null, 2);
     window.alert('Data successfully extracted. Check the console for the data.');
     console.log(jsonString);
   };
 
+  // Rendering the main component
   return (
     <div className="app">
       <h1>Book Donate</h1>
       {user ? (
+        // If user is authenticated, show welcome message, logout button, form, and table
         <div>
           <p>Welcome, {user.email}!</p>
           <button onClick={() => auth.signOut()} className="logoutbutton">Logout</button>
@@ -126,10 +147,12 @@ function App() {
           </div>
         </div>
       ) : (
+        // If user is not authenticated, show authentication component
         <Auth />
       )}
     </div>
   );
 }
 
+// Exporting the App component
 export default App;
